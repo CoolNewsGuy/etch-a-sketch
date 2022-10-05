0ce29a24ac01dec47efc83acb49b43a40ff31b91
MrLuckyCat <joewilliamdd31@gmail.com>
2022-10-05 07:44:00 +0100

    Put squares inside each line container

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let sketchContainer = document.getElementsByClassName("sketch-container")[0];
 function createLineContainers() {
     for (let i = 0; i < 16; i++) {
         let lineContainer = document.createElement("div");
-        lineContainer.style.height = sketchContainer.clientHeight / 16 + "px";
+        lineContainer.style.maxHeight =
+            sketchContainer.clientHeight / 16 + "px";
         lineContainer.style.backgroundColor = "red";
+        lineContainer.style.width = getComputedStyle(sketchContainer).width;
         lineContainer.classList.add("line-container");
         sketchContainer.appendChild(lineContainer);
     }
@@ -12,22 +14,33 @@ function createLineContainers() {
 
 function createOrangeSquares() {
     let orangeSquare = document.createElement("div");
+    orangeSquare.style.display = "inline-block";
     orangeSquare.style.background = "#fee715";
-    orangeSquare.style.width = "62.5px";
-    orangeSquare.style.minHeight = "62.5px";
-    orangeSquare.style.flex = "auto";
+    orangeSquare.style.width = sketchContainer.clientWidth / 16 + "px";
+    orangeSquare.style.height = sketchContainer.clientHeight / 16 + "px";
     return orangeSquare;
 }
 
 function createBlackSquares() {
     let blackSquare = document.createElement("div");
+    blackSquare.style.display = "inline-block";
     blackSquare.style.background = "#101820";
-    blackSquare.style.width = "62.5px";
-    blackSquare.style.minHeight = "62.5px";
-    blackSquare.style.flex = "auto";
+    blackSquare.style.width = sketchContainer.clientWidth / 16 + "px";
+    blackSquare.style.height = sketchContainer.clientHeight / 16 + "px";
     return blackSquare;
 }
 
 createLineContainers();
 
 let lineContainers = document.querySelectorAll(".line-container");
+lineContainers.forEach((container) => {
+    if (Array.from(lineContainers).indexOf(container) % 2 === 0) {
+        for (let i = 0; i < 8; i++) {
+            container.append(createOrangeSquares(), createBlackSquares());
+        }
+    } else if (Array.from(lineContainers).indexOf(container) % 2 !== 0) {
+        for (let i = 0; i < 8; i++) {
+            container.append(createBlackSquares(), createOrangeSquares());
+        }
+    }
+});
